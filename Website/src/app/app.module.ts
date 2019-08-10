c2ce5d96c4d4d77c7c798549e13f5ceb9c070eb0
smit2615 <ns23@umail.iu.edu>
2019-08-10 03:16:05 -0400

    Set up material

--- Website/src/app/app.module.ts
+++ Website/src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
+import { HeaderComponent } from './header/header.component';
+
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    HomeModule
+    HomeModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
